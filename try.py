10c786e535cde128a8a9ae77423adbce0c2dc4b0
Alex Rugina <alexrugina8@yahoo.com>
2025-04-07 15:48:00 +0300

    Screenshots for README

--- try.py
+++
@@ -1,5 +0,0 @@
-exemplu = "After the unexpected death of the Pope, Cardinal Lawrence is tasked with managing the covert and ancient ritual of electing a new one. Sequestered in the Vatican with the Catholic Church’s most powerful leaders until the process is complete, Lawrence finds himself at the center of a conspiracy that could lead to its downfall..."
-
-if "..." in exemplu:
-    exemplu_nou = exemplu.replace("...", ".")
-print(exemplu_nou)
